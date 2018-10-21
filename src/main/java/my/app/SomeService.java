f0951b981c48e1e448a55b56d0147a4eb842b536
Aditya Srinivasan <aditya.s@outlook.com>
2018-10-21 12:12:09 -0400

    Failing DataSource.

--- src/main/java/my/app/SomeService.java
+++ src/main/java/my/app/SomeService.java
@@ -1,7 +1,13 @@
 package my.app;
 
 import javax.inject.Singleton;
+import javax.sql.DataSource;
 
 @Singleton
 public class SomeService {
+	DataSource dataSource;
+	
+	public SomeService(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
 }
